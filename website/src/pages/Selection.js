04fe7344f7bec511d232836e618ec0f61a4b7f56
Lux <64970263+luxeu@users.noreply.github.com>
2025-04-12 14:19:39 -0400

    plans button templates

--- website/src/pages/Selection.js
+++ website/src/pages/Selection.js
@@ -1,53 +1,60 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import './Pages.css';
+import RouteButton from '../components/RouteButton';
 
 function Selection() {
-  const { navId } = useParams();
+    const { navId } = useParams();
 
-  let content;
+    let content;
 
-  switch (navId) {
-    case '251north':
-      content = (
-        <nav className="northbar">
-        <div>
-          <h1 className="north">251 North Selection</h1>      
-        </div>
-        <div className="dietimage">
-          <img src="./pics/vegs.png" alt="" ></img>
-          </div>
-        </nav>
-      );
-      break;
-    case 'yahentamitsi':
-      content = (
-        <div>
-          <h1>Yahentamitsi Selection</h1>
-          <p>This is the Y</p>
-          {/* Add content specific to Yahentamitsi */}
-        </div>
-      );
-      break;
-    case 'south':
-      content = (
-        <div>
-          <h1>South Campus Selection</h1>
-          <p>This is South</p>
-          {/* Add content specific to South Campus */}
-        </div>
-      );
-      break;
-    default:
-      content = (
-        <div>
-          <h1>Invalid Destination</h1>
-        </div>
-      );
-  }
+    switch (navId) {
+        case '251north':
+            content = (
+                <nav className="northbar">
+                    <div>
+                        <h1 className="north">251 North Selection</h1>
+                    </div>
+                    <div className="dietimage">
+                        <img src="./pics/vegs.png" alt="" ></img>
+                    </div>
+                    <RouteButton to="/selection/diet/${navId}" style={{ position: 'relative', top: '50px', left: '1000px'}}>
+                        Dietary Plans
+                    </RouteButton>
+                    <RouteButton to="/selection/calories/${navId}" style={{ position: 'relative', top: '350px', left: '80px'}}>
+                        Calorie Plans
+                    </RouteButton>
+                </nav>
+            );
+            break;
+        case 'yahentamitsi':
+            content = (
+                <div>
+                    <h1>Yahentamitsi Selection</h1>
+                    <p>This is the Y</p>
+                    {/* Add content specific to Yahentamitsi */}
+                </div>
+            );
+            break;
+        case 'south':
+            content = (
+                <div>
+                    <h1>South Campus Selection</h1>
+                    <p>This is South</p>
+                    {/* Add content specific to South Campus */}
+                </div>
+            );
+            break;
+        default:
+            content = (
+                <div>
+                    <h1>Invalid Destination</h1>
+                </div>
+            );
+    }
 
-  return <div>{
-    content
+    return <div>{
+        content
     }</div>;
 }
 
