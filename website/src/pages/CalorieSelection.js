2df028fe3513fbbea7d1556f3ff83a2f3a02d6b9
etah1 <familyta311@gmail.com>
2025-04-12 17:28:54 -0400

    dropdowns

--- website/src/pages/CalorieSelection.js
+++ website/src/pages/CalorieSelection.js
@@ -1,32 +1,77 @@
-import React from 'react';
-import './Pages.css';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
+import './Pages.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+
 
 function CalorieSelection() {
     const { navId } = useParams();
+    const [selectedRange, setSelectedRange] = useState(null);
+
+    const handleSelect = (eventKey) => {
+      setSelectedRange(eventKey);
+    };
 
     let content;
     
     switch (navId) {
         case '251north':
             content = (
-                <div>
-                    <h1> 251 Calorie Selection</h1>
-                </div>
+                <div className="northcal">
+                    <DropdownButton id="dropdown-basic-button" title="Calorie Ranges" onSelect={handleSelect}
+                    >
+      <Dropdown.Item eventKey="1500-2000 cal">1500-2000 cal</Dropdown.Item>
+      <Dropdown.Item eventKey="2000-2500 cal">2000-2500 cal</Dropdown.Item>
+      <Dropdown.Item eventKey="2500-3000 cal">2500-3000 cal</Dropdown.Item>
+      <Dropdown.Item eventKey="3000-3500 cal">3000-3500 cal</Dropdown.Item>
+
+    </DropdownButton>
+    {selectedRange && (
+            <div className="range">
+              <p>Showing meals for: {selectedRange}</p>
+            </div>
+          )}
+                </div>  
             );
             break;
         case 'yahentamitsi':
             content = (
-                <div>
-                    <h1>Yahentamitsi Calorie Selection</h1>
-                </div>
+                <div className="Ycal">
+                    <DropdownButton id="dropdown-basic-button" title="Calorie Ranges" onSelect={handleSelect}
+                    >
+      <Dropdown.Item eventKey="1500-2000 cal">1500-2000 cal</Dropdown.Item>
+      <Dropdown.Item eventKey="2000-2500 cal">2000-2500 cal</Dropdown.Item>
+      <Dropdown.Item eventKey="2500-3000 cal">2500-3000 cal</Dropdown.Item>
+      <Dropdown.Item eventKey="3000-3500 cal">3000-3500 cal</Dropdown.Item>
+
+    </DropdownButton>
+    {selectedRange && (
+            <div className="range">
+              <p>Showing meals for: {selectedRange}</p>
+            </div>
+          )}
+                </div>  
             );
             break;
         case 'south':
             content = (
-                <div>
-                    <h1>South Campus Calorie Selection</h1>
-                </div>
+                <div className="southcal">
+                    <DropdownButton id="dropdown-basic-button" title="Calorie Ranges" onSelect={handleSelect}
+                    >
+      <Dropdown.Item eventKey="1500-2000 cal">1500-2000 cal</Dropdown.Item>
+      <Dropdown.Item eventKey="2000-2500 cal">2000-2500 cal</Dropdown.Item>
+      <Dropdown.Item eventKey="2500-3000 cal">2500-3000 cal</Dropdown.Item>
+      <Dropdown.Item eventKey="3000-3500 cal">3000-3500 cal</Dropdown.Item>
+
+    </DropdownButton>
+    {selectedRange && (
+            <div className="range">
+              <p>Showing meals for: {selectedRange}</p>
+            </div>
+          )}
+                </div>  
             );
             break;
         default:
