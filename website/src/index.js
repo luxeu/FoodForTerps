feef1ccee5380a1fe809581720a11dbb13bd00b9
Lux <64970263+luxeu@users.noreply.github.com>
2025-04-12 00:25:25 -0400

    3 bar layout

--- website/src/index.js
+++ website/src/index.js
@@ -4,11 +4,13 @@ import './index.css';
 import App from './App';
 import NavBar from './NavBar';
 import reportWebVitals from './reportWebVitals';
+import ImageNavigation from './ImageNavigation';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <NavBar />
+    <NavBar/>
+    <ImageNavigation/>
   </React.StrictMode>
 );
 
