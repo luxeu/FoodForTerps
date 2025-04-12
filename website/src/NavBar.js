f5666a3add36701ad81cb2e0a124cf99be9c6cf9
etah1 <familyta311@gmail.com>
2025-04-11 23:38:00 -0400

    initial commit

---
+++ website/src/NavBar.js
@@ -0,0 +1,19 @@
+import './Home.css';
+
+function NavBar() {
+return(
+    <>
+    <nav className="navbar">
+      <div className="navbar-container">
+      <p> 
+        Food For Terps
+      </p>
+      <div className="navbar-container2">
+      </div>
+      </div>
+      </nav>
+    </>
+)
+}
+
+export default NavBar
