37071dd1462857afb0b7256bf544f2774aa86d4e
etah1 <familyta311@gmail.com>
2025-04-12 13:47:41 -0400

    251

--- website/src/pages/Selection.js
+++ website/src/pages/Selection.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import './Pages.css';
 
 function Selection() {
   const { navId } = useParams();
@@ -9,11 +10,14 @@ function Selection() {
   switch (navId) {
     case '251north':
       content = (
+        <nav className="northbar">
         <div>
-          <h1>251 North Selection</h1>
-          <p>This is 251 north</p>
-          {/* Add content specific to 251 North */}
+          <h1 className="north">251 North Selection</h1>      
         </div>
+        <div className="dietimage">
+          <img src="./pics/vegs.png" alt="" ></img>
+          </div>
+        </nav>
       );
       break;
     case 'yahentamitsi':
