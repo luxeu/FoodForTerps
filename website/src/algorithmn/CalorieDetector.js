63ffffd4ee17f148f99ad3f5d5d0ab1700c6c866
Panda248 <40081083+Panda248@users.noreply.github.com>
2025-04-12 19:30:12 -0400

    fix calorie access and added calorie range

--- website/src/algorithmn/CalorieDetector.js
+++ website/src/algorithmn/CalorieDetector.js
@@ -1,21 +1,40 @@
 import {generateFoodMap} from "./MongoGrabber.js";
 // const food = require('./MongoGrabber.js');
 
-async function CalorieDetector(time, location, index){
+async function CalorieDetector(time, location, index, calorieMin, calorieMax){
     // const foodList = await food.generateFoodMap(time, location);
     const foodList = await generateFoodMap(time, location);
-    console.log("Grabbing Grains");
-    const grain = foodList.get('grains')[index];
-    const fruits = foodList.get('fruits')[index];
-    const protein = foodList.get('proteins')[index];
+    // console.log("Grabbing Grains");
+    const grains = foodList.get('grains');
+    const fruits = foodList.get('fruits');
+    const proteins = foodList.get('proteins');
+    const length = Math.min(grains.length, fruits.length, proteins.length);
+    var grain, fruit, protein;
+    for (var i = 0; i < length; i++) {
+        var totalCalories = grains[i].nutrition.Calories + fruits[i].nutrition.Calories + proteins[i].nutrition.Calories;
+        console.log(grains[i].nutrition.Calories + " "+ fruits[i].nutrition.Calories +" "+ proteins[i].nutrition.Calories)
+        if( totalCalories >= calorieMin && totalCalories <= calorieMax) {
+            if (index == 0) {
+                console.log("found Match");
+                grain = grains[i];
+                fruit = fruits[i];
+                protein = proteins[i];
+                break;
+            }
+            else {
+                index--;
+            }
+        }
+    }
     const result = [];
     result[0] = grain;
-    result[1] = fruits;
+    result[1] = fruit;
     result[2] = protein;
     return result;
 
 }
 
-const fuck = await CalorieDetector("lunch", "yahen", 0);
+
+const fuck = await CalorieDetector("lunch", "yahen", 0, 500, 1000);
 console.log(fuck);
 
