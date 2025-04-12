42bf17e15c7d8b85ff74667e976320adedcc0bc8
Lux <64970263+luxeu@users.noreply.github.com>
2025-04-12 13:08:53 -0400

    new navbar routing

--- website/src/NavBar.js
+++ website/src/NavBar.js
@@ -1,19 +1,47 @@
+import { Link, useMatch, useResolvedPath } from "react-router-dom"
 import './Home.css';
 
 function NavBar() {
-return(
-    <>
-    <nav className="navbar">
-      <div className="navbar-container">
-      <p> 
-        Food For Terps
-      </p>
-      <div className="navbar-container2">
-      </div>
-      </div>
-      </nav>
-    </>
-)
+  return (
+    <nav className="nav">
+      <Link to="/" className="site-title">
+        FoodForTerps
+      </Link>
+      <ul>
+        <Link to="/aboutus">About Us</Link>
+        <Link to="/howto">How To</Link>
+      </ul>
+    </nav>
+  )
+}
+
+function CustomLink({ to, children, ...props }) {
+  const resolvedPath = useResolvedPath(to)
+  const isActive = useMatch({ path: resolvedPath.pathname, end: true })
+
+  return (
+    <li className={isActive ? "active" : ""}>
+      <Link to={to} {...props}>
+        {children}
+      </Link>
+    </li>
+  )
 }
 
 export default NavBar
+
+// function NavBar() {
+//   return (
+//     <nav className="navbar">
+//       <div className="navbar-container">
+//         <p>
+//           Food For Terps
+//         </p>
+//         <div className="navbar-container2">
+//         </div>
+//       </div>
+//     </nav>
+//   )
+// }
+
+// export default NavBar
