eeed9af48f893af8e473e0ef565ea0cb5a33bd8d
Lux <64970263+luxeu@users.noreply.github.com>
2025-04-12 00:34:51 -0400

    bug fix

--- website/src/index.js
+++ website/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import NavBar from './NavBar';
-import South from './South';
 import reportWebVitals from './reportWebVitals';
 import ImageNavigation from './ImageNavigation';
 
