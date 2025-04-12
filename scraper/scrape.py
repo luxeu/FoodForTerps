d841f6083857329d26d22c5176c61e5791b85c52
Panda248 <40081083+Panda248@users.noreply.github.com>
2025-04-11 23:27:50 -0400

    Update scrape.py

--- scraper/scrape.py
+++ scraper/scrape.py
@@ -1,1 +1,29 @@
 from bs4 import BeautifulSoup
+import urllib.request
+
+## Initializing values
+date = "Date" # TODO get date then format for url
+dining_hall = 19 # TODO get location id for url
+url = "https://nutrition.umd.edu/"
+webpage = urllib.request.urlopen(url)
+## Parse webpage into soup
+webpage_soup = BeautifulSoup(webpage, "html.parser")
+
+## Get cards
+body = webpage_soup.body
+cards = body.find_all("div", {"class": "card-body"})
+# cards = body.find_all("div", {"class": "row menu-item-row"})
+
+for child in cards:
+    location = child.find("h5", {"class": "card-title"})
+    print(location.string)
+    menu_items = child.find_all("div", {"class": "row menu-item-row"})
+    for item in menu_items:
+        name = item.find("a", {"class": "menu-item-name"})
+        print(name.string)
+        # record allergen/diet tags
+        diet_tags = item.findAll("img", {"class": "nutri-icon"})
+        for tag in diet_tags:
+            print(tag["title"])
+
+    
